9fb46fea558210072f4f9e5c6fec7d23b4c5ae1f
Akingbade675 <akingbadebasit26@gmail.com>
2022-09-22 23:13:26 +0000

    added python modules files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    n = len(argv) - 1
+    if n == 3:
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+        if operator not in ["+", "-", "*", "/"]:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        elif operator == "+":
+                print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif operator == "-":
+                print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif operator == "*":
+                print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        elif operator == "/":
+                print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv) - 1
+    print("{} {}{}".format(n, "arguments" if not n == 1 else "argument", ":" if n else "."))
+    if n:
+        for i in range(1, n + 1):
+            print("{}: {}".format(i, argv[i]))
