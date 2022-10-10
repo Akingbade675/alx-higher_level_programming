81687ba7155da2376640f1a3e836f863db10eee0
Akingbade675 <akingbadebasit26@gmail.com>
2022-10-10 23:57:39 +0000

    fixing exception bugs

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except TypeError as err:
+        return True
+    except (TypeError, ValueError) as err:
         print("Exception: {}".format(err), file=stderr)
-        return (False)
+        return False
