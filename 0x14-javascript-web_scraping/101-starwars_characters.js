77bf15f25e8525e576bb44c08fd875b2b8a2a937
Akingbade675 <akingbadebasit26@gmail.com>
2023-02-10 02:15:41 +0000

    fixed bug in async/await

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,37 @@
-#!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
+const async = require('async');
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, async (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const { characters } = JSON.parse(body);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-    for (const character of characters) {
-      console.log(character);
-      await request(character, (err, res, body) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const { name } = JSON.parse(body);
-        console.log(name);
-      });
-    }
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('An error occurred while trying to retrieve the data:', error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('An error occurred while trying to retrieve the data:', response.statusCode, response.statusMessage);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(`Characters in the movie "${movie.title}":`);
+  async.eachSeries(movie.characters, (characterUrl, callback) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('An error occurred while trying to retrieve the data:', error);
+        return callback(error);
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('An error occurred while trying to retrieve the data:', response.statusCode, response.statusMessage);
+        return callback(new Error(response.statusMessage));
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+      callback();
+    });
+  });
 });
